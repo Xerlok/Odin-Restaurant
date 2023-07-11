05a6e2ee76aa6339be9908acf8f2acdeb852bb43
Xerlok <20791727+Xerlok@users.noreply.github.com>
2023-07-11 22:54:52 +0300

    Create basic framework

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('This shit is getting intense...');
+console.log('Yeah...');
+console.log('...');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSxtQiIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4tcmVzdGF1cmFudC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnVGhpcyBzaGl0IGlzIGdldHRpbmcgaW50ZW5zZS4uLicpO1xyXG5jb25zb2xlLmxvZygnWWVhaC4uLicpO1xyXG5jb25zb2xlLmxvZygnLi4uJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
